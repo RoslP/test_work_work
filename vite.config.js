5bb4411c9fcaec123844a0e054b8e0c540523682
Pavel99 <ght12345ght12345@gmail.com>
2025-12-08 16:22:10 +1000

    Исправлена не работаящая "hot reload"

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,18 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         vue({
